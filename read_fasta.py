e6738c2d29bc75d1d715bdbf5304649952ff9202
Terminika <Jkfleirb_cj_cvtnfyrjq_500>
2023-11-26 19:18:49 +0300

    improve fasta file

--- read_fasta.py
+++ read_fasta.py
@@ -1,14 +1,8 @@
 def read_fasta(path):
-    with open (path) as f:
-        sq_name = ''
-        sq = ''
+    with open(path) as f:
         lines = f.readlines()
         sq_name = lines[0]
-        for line in lines[1:]:
-            sq += line
-        l = []
-        l.append(sq_name)
-        l.append(sq)
-        return l
+        sq = ''.join(lines[:1])
+        return sq_name, sq
 
 #print(read_fasta('/home/qrewetka/Rabin-Karp-algorithm/example.fa'))
