79edbc0a9693dc87b494f36e911940879093d1bc
Terminika <Jkfleirb_cj_cvtnfyrjq_500>
2023-11-26 17:19:28 +0300

    beautified rca func

--- RCAlg.py
+++ RCAlg.py
@@ -4,35 +4,52 @@ p = 20
 alphabet = {"A": 0, "C": 1, "G": 2, "T": 3}
 
 
-def hashPoly(S): #проблема с хэш-функцией
+def hashPoly(S):  # проблема с хэш-функцией
     
     h = sum(alphabet[S[c]]*(p**c) for c in range(len(S)))
     return h
 
 
-def Rabin_Carp_Alg(DNA,target):
-    if len(target) > len(DNA):
+def Rabin_Carp_Alg(DNA, target):
+    """
+    param DNA: initial sequence
+    param target: substring
+    return: list of indexes where the subsequence is
+    """
+
+    len_target = len(target)
+    len_DNA = len(DNA)
+
+    if len_target > len_DNA:
         raise Exception('Wrong target sequence')
     
     if not isinstance(DNA, str) or not isinstance(target, str):
         raise TypeError("Wrong data type")
-    
-    answer = []
-    DNA_hash = hashPoly(DNA[:len(target)])
-    target_hash = hashPoly(target) #const
 
-    for i in range(len(DNA)-len(target)+1):
+    answer = []
+    target_hash = hashPoly(target)  # is constant
+
+    for i in range(len_DNA - len_target + 1):
+        subseq = DNA[i:i + len_target]
+        subseq_hash = hashPoly(subseq)
+
         print(i)
-        print(DNA_hash)
+        print(subseq_hash)
         print(target_hash)
-        print(DNA[i:i+len(target)])
+        print(DNA[i:i + len_target])
         print(target)
-        if DNA_hash == target_hash and DNA[i:i+len(target)] == target:
-            answer.append(i)
-        #DNA_hash = hashPoly(DNA[i+1:i+1+len(target)])
-        DNA_hash = (DNA_hash - pow(p,len(target))*alphabet[DNA[i]])/p + alphabet[DNA[i+len(target)]]
+
+        if subseq_hash == target_hash:
+            equal = True
+            for j in range(len_target):
+                if subseq[j] != target[j]:
+                    equal = False
+                    break
+            if equal:
+                answer.append(i)
 
     return answer
+
 
 DNA = 'ACGTAAACGT'
 target = 'CGT'
